f8ed85c52337d4af5675e6111e1393c188e1242d
PMaciusiak <61948835+PMaciusiak@users.noreply.github.com>
2021-03-10 17:37:39 +0100

    Initial commit

---
+++ settings.py
@@ -0,0 +1,13 @@
+class Settings:
+    def __init__(self):
+        """inicjalizacja ustawien gry"""
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        self.ship_speed = 1.5
+
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (0, 0, 0)
+        self.bullets_allowed = 5
